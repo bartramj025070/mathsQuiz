555fec2523ffc38b8c255d40836fb569bcfb1d12
Jasper Bartram <134063226+bartramj025070@users.noreply.github.com>
2026-06-09 09:56:09 +0100

    Add files via upload

--- Helpers/XMLReader.py
+++ Helpers/XMLReader.py
@@ -1,6 +1,7 @@
 ## XMLReader.py
 ## Parses basic XML syntax through custom ruling for "pages"
 import os
+import xml.etree.ElementTree as XML
 
 directory = os.path.dirname(__file__)
 root = os.path.dirname(directory)
@@ -20,9 +21,16 @@ class XMLTag:
     def __init__(self, tagName, attributes):
         self.tagName=tagName
         self.attributes=attributes
+        self.tags=[]
+        
+    def addTag(self, tag):
+        self.tags.append(tag)
 
 class XMLFile:
     def __init__(self, pageName):
         self.pageName=pageName
-        self.content=ReadPage(pageName)
-        self.tags=[]
+    
+    def get(self):
+        tree = XML.parse(os.path.join(pagesDir, self.pageName) + ".xml")
+        root = tree.getroot()
+        return root
--- MathsQuiz [DEBUG].py
+++ MathsQuiz [DEBUG].py
@@ -2,6 +2,25 @@
 import pygame
 import time
 import random
+from Helpers import XMLReader
+
+WINDOW_NAME = "Maths Quiz"
+WINDOW_CONSTRUCTORS = {}
+
+## Functions
+### https://stackoverflow.com/questions/29643352/converting-hex-to-rgb-value-in-python
+def hex_to_rgb(value):
+    value = value.lstrip('#')
+    lv = len(value)
+    return tuple(int(value[i:i + lv // 3], 16) for i in range(0, lv, lv // 3))
+
+def WindowConstructor(func):
+    def Wrapper(constructorName, parameters):
+        WINDOW_CONSTRUCTORS[constructorName] = {
+            "func": func,
+            
+        }
+    return Wrapper
 
 ## Pygame Setup
 pygame.init()
@@ -12,11 +31,26 @@ ScreenH = 500
 Screen = pygame.display.set_mode( (ScreenW, ScreenH) )
 Clock = pygame.time.Clock()
 
+CurrentPage = 'LoginPage'
+
+pygame.display.set_caption(WINDOW_NAME)
+
 __shouldRun = True
 while __shouldRun:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             __shouldRun = False
+
+    xmlContent = XMLReader.XMLFile(CurrentPage).get()
+    attributes = xmlContent.attrib
+    for key in attributes:
+        if key == "windowPostfix":
+            postfix = attributes[key]
+            pygame.display.set_caption(WINDOW_NAME + ": " + postfix)
+        if key == "background":
+            hexCol = attributes[key]
+            Screen.fill(hex_to_rgb(hexCol))
+            
 
     pygame.display.flip() ## Effectively a blit
     Clock.tick(60)
